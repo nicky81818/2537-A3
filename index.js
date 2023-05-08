dae5c57780a27c17d6775efb01cded8d30362a61
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-05-08 16:08:36 -0700

    test axios

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
-const setup = () => {
-    console.log("Hello World!")
+const setup = async () => {
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=810')
+    console.log(res.data)
 }
 
 $(document).ready(setup)
