5bc691bc6085db27eca030663c0b1f10703c1180
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-05-08 16:19:39 -0700

    get all the pokemon into cards

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 
 const setup = async () => {
     const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=810')
-    console.log(res.data)
+    // console.log(res.data)
+    const pokemons = res.data.results
+    pokemons.forEach(async pokemon => {
+        const res = await axios.get(pokemon.url)
+        $('#pokeCards').append(`
+            <div class="card text-center" style="width: 18rem;">
+                <h2 class="card-title">${pokemon.name.toUpperCase()}</h2>
+                <img src="${res.data.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
+                <div class="card-body">
+                    <button class="btn btn-primary" id="${pokemon.name}">More</button>
+                </div>
+            </div>
+        `)
+    })
 }
 
 $(document).ready(setup)
